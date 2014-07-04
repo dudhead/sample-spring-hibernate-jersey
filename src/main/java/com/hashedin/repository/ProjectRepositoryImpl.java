3e28079bd85413f80232f0b1eb57310679008358
karthik <karthik.k@hashedin.com>
2014-07-04 16:46:39 +0530

    Added repository classes

---
+++ src/main/java/com/hashedin/repository/ProjectRepositoryImpl.java
@@ -0,0 +1,30 @@
+package com.hashedin.repository;
+
+import java.util.List;
+
+import com.hashedin.model.Project;
+
+public class ProjectRepositoryImpl implements ProjectRepository {
+
+	@Override
+	public Project find(Long projectId) {
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Project> findAll() {
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Project save(Project project) {
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/hashedin/repository/UserRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.hashedin.repository;
+
+import com.hashedin.model.User;
+import com.hashedin.service.UserService;
+
+public class UserRepositoryImpl implements UserService {
+
+	@Override
+	public User find(Long userId) {
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User save(User user) {
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
